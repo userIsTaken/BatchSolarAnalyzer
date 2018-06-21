5e1475c87eab440fe5a5b5bf725b9b8468659082
userIsTaken <34156048+userIsTaken@users.noreply.github.com>
2018-06-21 18:50:29 +0300

    Initial commit

---
+++ batchSolarCellAnalyzer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+from PyQt5 import QtWidgets, QtGui
+from UIfiles.BSA_MainGui import Ui_MainWindow
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        # SLOTS:
+        self.ui.closeAppButton.clicked.connect(self.closeFunction)
+        self.ui.actionClose.triggered.connect(self.closeFunction)
+        self.ui.addFilesButton.clicked.connect(self.addFilesFunction)
+        self.ui.clearTableButton.clicked.connect(self.clearTableFunction)
+
+    def ConsoleOutput(self, text=None):
+        if text is not None:
+            self.ui.consoleOutput.appendPlainText(str(text))
+        else:
+            pass
+
+    def clearTableFunction(self):
+        self.ConsoleOutput("Table will be cleared")
+        pass
+
+
+    def closeFunction(self):
+        print("Exiting ... ")
+        sys.exit(10)
+        pass
+
+    def addFilesFunction(self):
+        self.ConsoleOutput("FILES WILL BE ADDED")
+        pass
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
